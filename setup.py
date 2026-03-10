399927f9b2599bfbfd26bc33d71081f28ca3b78f
Dr. Aleksei Krasnov <dr.aleksei.krasnov@gmail.com>
2026-03-10 20:57:52 +0100

    chore: verson in setup from __init__

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+# Read the version from __init__.py
+with open("chemic/__init__.py", "r", encoding="utf-8") as fh:
+    exec(fh.read())
+
 def read_requirements():
     """Read the requirements.txt file and return a list of dependencies."""
     with open("requirements.txt", "r", encoding="utf-8") as fh:
@@ -12,7 +16,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ChemIC-ml",
-    version="1.3.1",
+    version=__version__,
     description="Chemical images classification project. Program for training the deep neural network model and web service for classification  chemical images",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -22,7 +26,6 @@ setup(
     python_requires=">=3.10,<3.13",
     classifiers=[
         "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
